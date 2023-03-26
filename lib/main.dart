1a212449781a381ae553a358d539498158a9b41f
foad9776 <F_137071f>
2023-03-26 18:41:03 +0300

    main page with icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:itec404_delivery_app/pages/home.dart';
+import 'package:itec404_delivery_app/pages/mainPage.dart';
 import 'package:itec404_delivery_app/pages/register.dart';
 import 'package:itec404_delivery_app/pages/login.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -9,11 +10,13 @@ void main() async {
   await Firebase.initializeApp();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    initialRoute: '/',
+    initialRoute: '/mainPage',
     routes: {
       '/': (context) => HomeScreen(),
       '/register': (context) => RegisterPage(),
       '/login': (context) => LoginPage(),
+      '/mainPage': (context) => MainPage(),
+
     },
   ));
 }
