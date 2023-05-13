69f21d109b199105675a336b4524cf95e359b7ee
foad9776 <F_137071f>
2023-05-13 21:24:04 +0300

    upload image fixed from front end side

--- lib/pages/order.dart
+++ lib/pages/order.dart
@@ -27,7 +27,9 @@ class _Order extends State<OrderPage>with TickerProviderStateMixin {
   String? _userId;
   String? _Name;
 
+  String _image = 'https://ouch-cdn2.icons8.com/84zU-uvFboh65geJMR5XIHCaNkx-BZ2TahEpE9TpVJM/rs:fit:784:784/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvODU5/L2E1MDk1MmUyLTg1/ZTMtNGU3OC1hYzlh/LWU2NDVmMWRiMjY0/OS5wbmc.png';
   late AnimationController loadingController;
+
   File? _file;
   PlatformFile? _platformFile;
 
@@ -49,6 +51,11 @@ class _Order extends State<OrderPage>with TickerProviderStateMixin {
 
   @override
   void initState() {
+    loadingController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 10),
+    )..addListener(() { setState(() {}); });
+    super.initState();
     super.initState();
     getNameFromSharedPreferences();
     _getUserData();
@@ -226,15 +233,11 @@ class _Order extends State<OrderPage>with TickerProviderStateMixin {
                 ),
               ),
               //Image.network(_image, width: 300,),
-              SizedBox(height: 50,),
-              Text('Upload your file', style: TextStyle(fontSize: 25, color: Colors.grey.shade800, fontWeight: FontWeight.bold),),
               SizedBox(height: 10,),
-              Text('File should be jpg, png', style: TextStyle(fontSize: 15, color: Colors.grey.shade500),),
-              SizedBox(height: 20,),
               GestureDetector(
                 onTap: selectFile,
                 child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+                    padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
                     child: DottedBorder(
                       borderType: BorderType.RRect,
                       radius: Radius.circular(10),
@@ -243,7 +246,7 @@ class _Order extends State<OrderPage>with TickerProviderStateMixin {
                       color: Colors.blue.shade400,
                       child: Container(
                         width: double.infinity,
-                        height: 150,
+                        height: 100,
                         decoration: BoxDecoration(
                             color: Colors.blue.shade50.withOpacity(.3),
                             borderRadius: BorderRadius.circular(10)
@@ -251,21 +254,81 @@ class _Order extends State<OrderPage>with TickerProviderStateMixin {
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Icon(Iconsax.folder_open, color: Colors.blue, size: 40,),
-                            SizedBox(height: 15,),
+                            Icon(Iconsax.folder_open, color: Colors.blue, size: 35,),
+                            SizedBox(height: 5),
                             Text('Select your file', style: TextStyle(fontSize: 15, color: Colors.grey.shade400),),
+                              Text('File should be jpg, png', style: TextStyle(fontSize: 10, color: Colors.grey.shade500),),
                           ],
                         ),
                       ),
                     ),
                 ),
               ),
-              ElevatedButton(
-                onPressed: () {
+              _platformFile != null
+                  ? Container(
+                  padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Selected File',
+                        style: TextStyle(color: Colors.grey.shade400, fontSize: 15, ),),
+                      SizedBox(height: 10,),
+                      Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.shade200,
+                                  offset: Offset(0, 1),
+                                  blurRadius: 3,
+                                  spreadRadius: 2,
+                                )
+                              ]
+                          ),
+                          child: Row(
+                            children: [
+                              ClipRRect(
+                                  borderRadius: BorderRadius.circular(8),
+                                  child: Image.file(_file!, width: 70,)
+                              ),
+                              SizedBox(width: 10,),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(_platformFile!.name,
+                                      style: TextStyle(fontSize: 13, color: Colors.black),),
+                                    SizedBox(height: 5,),
+                                    Text('${(_platformFile!.size / 1024).ceil()} KB',
+                                      style: TextStyle(fontSize: 13, color: Colors.grey.shade500),
+                                    ),
+                                    SizedBox(height: 5,),
+                                    Container(
+                                        height: 5,
+                                        clipBehavior: Clip.hardEdge,
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(5),
+                                          color: Colors.blue.shade50,
+                                        ),
+                                        child: LinearProgressIndicator(
+                                          value: loadingController.value,
+                                        )
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(width: 10,),
+                            ],
+                          )
+                      ),
+                    ],
+                  ))
+                  : Container(),
 
-                },
-                child: Text('Get Image'),
-              ),
+
+
               ElevatedButton(
                 onPressed: _createOrder,
                 child: Text('Create Order'),
