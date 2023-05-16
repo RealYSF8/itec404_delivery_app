e48160586a57e378b5b5305eed26fda8829271fc
hadd1 <damnmodzz@gmail.com>
2023-05-16 21:46:42 +0300

    general permissions modification

--- lib/pages/more.dart
+++ lib/pages/more.dart
@@ -4,14 +4,13 @@ import 'package:shared_preferences/shared_preferences.dart';
 class More extends StatefulWidget {
   @override
   _MoreState createState() => _MoreState();
-
 }
 
 class _MoreState extends State<More> {
   String name = "";
   int _selectedIndex = 2;
   static const TextStyle optionStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static const List<Widget> _widgetOptions = <Widget>[
     Text(
       'Home Page',
@@ -30,12 +29,11 @@ class _MoreState extends State<More> {
   bool _isAdmin = false;
   bool _isCourier = false;
 
-
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
-    switch(index) {
+    switch (index) {
       case 0:
         Navigator.pushNamed(context, '/mainPage');
         break;
@@ -47,6 +45,7 @@ class _MoreState extends State<More> {
         break;
     }
   }
+
   @override
   void initState() {
     super.initState();
@@ -65,6 +64,7 @@ class _MoreState extends State<More> {
       _isAdmin = role == 'Admin';
     });
   }
+
   void _checkDeliveryStatus() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String role = prefs.getString('role') ?? '';
@@ -82,47 +82,54 @@ class _MoreState extends State<More> {
 
   @override
   Widget build(BuildContext context) {
-    showCustomAlert() => showDialog(context: context, builder: (context)=>AlertDialog(
-      title: Text("Are you sure?"),
-      content: Text("Log out?"),
-      actions: [
-        TextButton(onPressed: (){ return Navigator.pop(context, false);}, child: Text("Cancel")),
-        TextButton(onPressed: (){ return Navigator.pop(context, true);}, child: Text("Yes")),
-      ],
-    )
-
-    );
+    showCustomAlert() => showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: Text("Are you sure?"),
+              content: Text("Log out?"),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      return Navigator.pop(context, false);
+                    },
+                    child: Text("Cancel")),
+                TextButton(
+                    onPressed: () {
+                      return Navigator.pop(context, true);
+                    },
+                    child: Text("Yes")),
+              ],
+            ));
 
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         automaticallyImplyLeading: false,
         centerTitle: false,
-        title: Row(
-            children: <Widget>[
-              Text(
-                "My",
-                textAlign: TextAlign.start,
-                overflow: TextOverflow.clip,
-                style: TextStyle(
-                  fontWeight: FontWeight.w700,
-                  fontStyle: FontStyle.normal,
-                  fontSize: 22,
-                  color: Color(0xffffffff),
-                ),
-              ),
-              Text("Options",
-                textAlign: TextAlign.start,
-                overflow: TextOverflow.clip,
-                style: TextStyle(
-                  fontWeight: FontWeight.w700,
-                  fontStyle: FontStyle.normal,
-                  fontSize: 22,
-                  color: Color(0xfffba808),
-                ),
-              ),
-            ]
-        ),
+        title: Row(children: <Widget>[
+          Text(
+            "My",
+            textAlign: TextAlign.start,
+            overflow: TextOverflow.clip,
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontStyle: FontStyle.normal,
+              fontSize: 22,
+              color: Color(0xffffffff),
+            ),
+          ),
+          Text(
+            "Options",
+            textAlign: TextAlign.start,
+            overflow: TextOverflow.clip,
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontStyle: FontStyle.normal,
+              fontSize: 22,
+              color: Color(0xfffba808),
+            ),
+          ),
+        ]),
         backgroundColor: Colors.blue,
       ),
       body: Padding(
@@ -180,7 +187,7 @@ class _MoreState extends State<More> {
               iconBackgroundColor: Colors.grey[700]!,
               text: "Dark Mode",
             ),
-            SizedBox(height:15),
+            SizedBox(height: 15),
             GestureDetector(
               onTap: () {
                 Navigator.pushNamed(context, '/courrier');
@@ -198,13 +205,14 @@ class _MoreState extends State<More> {
               onTap: () {
                 Navigator.pushNamed(context, '/changepass');
               },
-              child :buildRowWithIconAndText(
+              child: buildRowWithIconAndText(
                 icon: Icons.build,
                 iconColor: Colors.white,
                 iconBackgroundColor: Colors.lightBlue[800]!,
                 text: "Change password",
                 showTrailingIcon: true,
-              ),),
+              ),
+            ),
             const SizedBox(height: 15),
             GestureDetector(
               onTap: () {
@@ -216,27 +224,29 @@ class _MoreState extends State<More> {
                 iconBackgroundColor: Colors.lightGreen,
                 text: "About us",
                 showTrailingIcon: true,
-              ),),
+              ),
+            ),
             const SizedBox(height: 15),
             GestureDetector(
               onTap: () {
                 Navigator.pushNamed(context, '/contact');
               },
-              child:buildRowWithIconAndText(
+              child: buildRowWithIconAndText(
                 icon: Icons.call,
                 iconColor: Colors.white,
                 iconBackgroundColor: Colors.purple,
                 text: "Contact Us",
                 showTrailingIcon: true,
-              ),),
+              ),
+            ),
             const SizedBox(height: 15),
             GestureDetector(
               onTap: () async {
-
                 var stat = await showCustomAlert();
-                if(stat){
+                if (stat) {
                   Navigator.pushNamedAndRemoveUntil(context, "/", (_) => false);
-                  SharedPreferences prefs = await SharedPreferences.getInstance();
+                  SharedPreferences prefs =
+                      await SharedPreferences.getInstance();
 
                   await prefs.setBool("isLoggedIn", false);
                   await prefs.setBool("isadmin", false);
@@ -248,11 +258,7 @@ class _MoreState extends State<More> {
                   await prefs.setString('category', "");
                   await prefs.setString('email', "");
                   await prefs.setString('address', "");
-
-
-
                 }
-
               },
               child: buildRowWithIconAndText(
                 icon: Icons.logout,
@@ -262,21 +268,22 @@ class _MoreState extends State<More> {
                 showTrailingIcon: true,
               ),
             ),
-            if (_isAdmin) Container(
-              margin: EdgeInsets.only(top: 15),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.pushNamed(context, '/admin');
-                },
-                child: buildRowWithIconAndText(
-                  icon: Icons.admin_panel_settings,
-                  iconColor: Colors.white,
-                  iconBackgroundColor: Colors.deepPurpleAccent,
-                  text: "Admin Panel",
-                  showTrailingIcon: true,
+            if (_isAdmin)
+              Container(
+                margin: EdgeInsets.only(top: 15),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, '/admin');
+                  },
+                  child: buildRowWithIconAndText(
+                    icon: Icons.admin_panel_settings,
+                    iconColor: Colors.white,
+                    iconBackgroundColor: Colors.deepPurpleAccent,
+                    text: "Admin Panel",
+                    showTrailingIcon: true,
+                  ),
                 ),
               ),
-            ),
             Container(
               margin: EdgeInsets.only(top: 15),
               child: GestureDetector(
@@ -292,26 +299,24 @@ class _MoreState extends State<More> {
                 ),
               ),
             ),
-            if (_isCourier) Container(
-              margin: EdgeInsets.only(top: 15),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.pushNamed(context, '/courier');
-                },
-                child: buildRowWithIconAndText(
-                  icon: Icons.local_shipping,
-                  iconColor: Colors.white,
-                  iconBackgroundColor: Colors.deepPurpleAccent,
-                  text: "Courier Panel",
-                  showTrailingIcon: true,
+            if (_isCourier)
+              Container(
+                margin: EdgeInsets.only(top: 15),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, '/courier');
+                  },
+                  child: buildRowWithIconAndText(
+                    icon: Icons.local_shipping,
+                    iconColor: Colors.white,
+                    iconBackgroundColor: Colors.deepPurpleAccent,
+                    text: "Courier Panel",
+                    showTrailingIcon: true,
+                  ),
                 ),
               ),
-            ),
           ],
-
-
         ),
-
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
@@ -367,7 +372,5 @@ class _MoreState extends State<More> {
           Icon(Icons.arrow_forward_ios, color: Colors.grey[600], size: 18),
       ],
     );
-
   }
 }
-
