956e3d87c1c0fa6b657434bbe1b48984c27e1f46
aboudehalimi <boudihalimi@gmail.com>
2023-05-17 11:51:11 +0300

    This is the idea for the order page for now

--- lib/pages/order.dart
+++ lib/pages/order.dart
@@ -13,180 +13,14 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class OrderPage extends StatefulWidget {
-
   @override
-  State<OrderPage> createState() => _Order();
-
+  _OrderPageState createState() => _OrderPageState();
 }
 
-class _Order extends State<OrderPage>with TickerProviderStateMixin {
-  final FirebaseStorage _storage = FirebaseStorage.instance;
-  File? _selectedImage;
-
-  Future<String> _uploadImage() async {
-    try {
-      FirebaseStorage storage = FirebaseStorage.instance;
-      Reference storageReference = storage.refFromURL('gs://itec404deliveryapp.appspot.com');
-      String fileName = Path.basename(_selectedImage!.path);
-      Reference imageReference = storageReference.child('images/$fileName');
-      UploadTask uploadTask = imageReference.putFile(_selectedImage!);
-
-      TaskSnapshot taskSnapshot = await uploadTask;
-
-      String imageUrl = await taskSnapshot.ref.getDownloadURL();
-      print("Image uploaded successfully. Download URL: $imageUrl");
-
-      return imageUrl;
-    } catch (e) {
-      print("Error uploading image: $e");
-      throw e; // Re-throw the error to be handled by the caller
-    }
-  }
-
-
-
-  Future<void> _pickImage() async {
-    try {
-      PickedFile? pickedFile = await ImagePicker().getImage(source: ImageSource.gallery);
-      if (pickedFile != null) {
-        setState(() {
-          _selectedImage = File(pickedFile.path);
-        });
-        print("Selected image: $_selectedImage");
-      }
-    } on PlatformException catch (e) {
-      print('Error picking image: $e');
-    }
-  }
-
-
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final FirebaseFirestore _db = FirebaseFirestore.instance;
-  final TextEditingController fromController = TextEditingController();
-  final TextEditingController toController = TextEditingController();
-  final TextEditingController lengthController = TextEditingController();
-  final TextEditingController widthController = TextEditingController();
-  final TextEditingController heightController = TextEditingController();
-
-  String? _userId;
-  String? _Name;
-
-  String _image = 'https://ouch-cdn2.icons8.com/84zU-uvFboh65geJMR5XIHCaNkx-BZ2TahEpE9TpVJM/rs:fit:784:784/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvODU5/L2E1MDk1MmUyLTg1/ZTMtNGU3OC1hYzlh/LWU2NDVmMWRiMjY0/OS5wbmc.png';
-  late AnimationController loadingController;
-
-  File? _file;
-  PlatformFile? _platformFile;
-
-  selectFile() async {
-    final file = await FilePicker.platform.pickFiles(
-        type: FileType.custom,
-        allowedExtensions: ['png', 'jpg', 'jpeg']
-    );
-
-    if (file != null) {
-      setState(() {
-        _file = File(file.files.single.path!);
-        _platformFile = file.files.first;
-      });
-    }
-
-    loadingController.forward();
-  }
-
-  @override
-  void initState() {
-    loadingController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 10),
-    )..addListener(() { setState(() {}); });
-    super.initState();
-    super.initState();
-    getNameFromSharedPreferences();
-    _getUserData();
-  }
-
-  void getNameFromSharedPreferences() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      _Name = prefs.getString("name") ?? "";
-    });
-  }
-
-  Future<void> _getUserData() async {
-    User? user = _auth.currentUser;
-    if (user != null) {
-      setState(() {
-        _userId = user.uid;
-      });
-    }
-  }
-
-  Future<void> _createOrder() async {
-    // Get the data from the input fields
-    String? from = fromController.text;
-    String? to = toController.text;
-    String? length = lengthController.text;
-    String? width = widthController.text;
-    String? height = heightController.text;
-
-    // Upload the image before creating the order
-    String? imageUrl = await _uploadImage();
-
-    // Get the current date and time
-    Timestamp now = Timestamp.now();
-
-    // Create a new order document with the data and timestamp
-    await _db.collection('orders').add({
-      'userId': _userId,
-      'Name': _Name,
-      'from': from,
-      'to': to,
-      'length': length,
-      'width': width,
-      'height': height,
-      'status': 'pending',
-      'createdAt': now, // add the current date and time to the document
-      'imageUrl': imageUrl, // use the image URL from Firebase Storage
-    }).then((value) {
-      // Display the order status in the database
-      _db.collection('orders').doc(value.id).update({'status': 'pending'});
-    });
-
-    // Clear the input fields
-    fromController.clear();
-    toController.clear();
-    lengthController.clear();
-    widthController.clear();
-    heightController.clear();
-
-    // Clear the selected image
-    setState(() {
-      _selectedImage = null;
-    });
-  }
-
-
-
-
-
-
+class _OrderPageState extends State<OrderPage> {
   int _selectedIndex = 1;
   static const TextStyle optionStyle =
   TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Home Page',
-      style: optionStyle,
-    ),
-    Text(
-      'Order Page',
-      style: optionStyle,
-    ),
-    Text(
-      'Account',
-      style: optionStyle,
-    ),
-  ];
 
   void _onItemTapped(int index) {
     setState(() {
@@ -212,186 +46,32 @@ class _Order extends State<OrderPage>with TickerProviderStateMixin {
         automaticallyImplyLeading: false,
         centerTitle: false,
         title: Row(
-            children: <Widget>[
-              Text(
-                "My",
-                textAlign: TextAlign.start,
-                overflow: TextOverflow.clip,
-                style: TextStyle(
-                  fontWeight: FontWeight.w700,
-                  fontStyle: FontStyle.normal,
-                  fontSize: 22,
-                  color: Color(0xffffffff),
-                ),
+          children: <Widget>[
+            Text(
+              "My",
+              textAlign: TextAlign.start,
+              overflow: TextOverflow.clip,
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontStyle: FontStyle.normal,
+                fontSize: 22,
+                color: Color(0xffffffff),
               ),
-              Text("Orders",
-                textAlign: TextAlign.start,
-                overflow: TextOverflow.clip,
-                style: TextStyle(
-                  fontWeight: FontWeight.w700,
-                  fontStyle: FontStyle.normal,
-                  fontSize: 22,
-                  color: Color(0xfffba808),
-                ),
+            ),
+            Text(
+              "Orders",
+              textAlign: TextAlign.start,
+              overflow: TextOverflow.clip,
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontStyle: FontStyle.normal,
+                fontSize: 22,
+                color: Color(0xfffba808),
               ),
-            ]
+            ),
+          ],
         ),
         backgroundColor: Colors.blue,
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: Column(
-            children: [
-              TextFormField(
-                controller: fromController,
-                decoration: const InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'From',
-                  suffixIcon: Icon(Icons.navigation_sharp),
-                  suffix: Text('Source'),
-                ),
-              ),
-              TextFormField(
-                controller: toController,
-                decoration: const InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'To',
-                  suffixIcon: Icon(Icons.assistant_navigation),
-                  suffix: Text('Destination'),
-                ),
-              ),
-              TextFormField(
-                controller: lengthController,
-                decoration: const InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'Length',
-                  suffixIcon: Icon(Icons.straighten),
-                  suffix: Text('CM'),
-                ),
-              ),
-              TextFormField(
-                controller: widthController,
-                decoration: const InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'Width',
-                  suffixIcon: Icon(Icons.straighten),
-                  suffix: Text('CM'),
-                ),
-              ),
-              TextFormField(
-                controller: heightController,
-                decoration: const InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'Height',
-                  suffixIcon: Icon(Icons.straighten),
-                  suffix: Text('CM'),
-                ),
-              ),
-              //Image.network(_image, width: 300,),
-              SizedBox(height: 10,),
-              GestureDetector(
-                onTap: selectFile,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-                  child: DottedBorder(
-                    borderType: BorderType.RRect,
-                    radius: Radius.circular(10),
-                    dashPattern: [10, 4],
-                    strokeCap: StrokeCap.round,
-                    color: Colors.blue.shade400,
-                    child: Container(
-                      width: double.infinity,
-                      height: 100,
-                      decoration: BoxDecoration(
-                          color: Colors.blue.shade50.withOpacity(.3),
-                          borderRadius: BorderRadius.circular(10)
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Iconsax.folder_open, color: Colors.blue, size: 35,),
-                          SizedBox(height: 5),
-                          Text('Select your file', style: TextStyle(fontSize: 15, color: Colors.grey.shade400),),
-                          Text('File should be jpg, png', style: TextStyle(fontSize: 10, color: Colors.grey.shade500),),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              _platformFile != null
-                  ? Container(
-                  padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Selected File',
-                        style: TextStyle(color: Colors.grey.shade400, fontSize: 15, ),),
-                      SizedBox(height: 10,),
-                      Container(
-                          padding: EdgeInsets.all(8),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.white,
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.grey.shade200,
-                                  offset: Offset(0, 1),
-                                  blurRadius: 3,
-                                  spreadRadius: 2,
-                                )
-                              ]
-                          ),
-                          child: Row(
-                            children: [
-                              ClipRRect(
-                                  borderRadius: BorderRadius.circular(8),
-                                  child: Image.file(_file!, width: 70,)
-                              ),
-                              SizedBox(width: 10,),
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(_platformFile!.name,
-                                      style: TextStyle(fontSize: 13, color: Colors.black),),
-                                    SizedBox(height: 5,),
-                                    Text('${(_platformFile!.size / 1024).ceil()} KB',
-                                      style: TextStyle(fontSize: 13, color: Colors.grey.shade500),
-                                    ),
-                                    SizedBox(height: 5,),
-                                    Container(
-                                        height: 5,
-                                        clipBehavior: Clip.hardEdge,
-                                        decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.circular(5),
-                                          color: Colors.blue.shade50,
-                                        ),
-                                        child: LinearProgressIndicator(
-                                          value: loadingController.value,
-                                        )
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              SizedBox(width: 10,),
-                            ],
-                          )
-                      ),
-                    ],
-                  ))
-                  : Container(),
-
-
-
-              ElevatedButton(
-                onPressed: _createOrder,
-                child: Text('Create Order'),
-              ),
-            ],
-          ),
-        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
@@ -411,8 +91,99 @@ class _Order extends State<OrderPage>with TickerProviderStateMixin {
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.amber[800],
         onTap: _onItemTapped,
+      ),
+      body: ListView(
+        children: [
+          MyCardClass(),
+          MyCardClass(),
+          MyCardClass(),
+          MyCardClass(),
+          MyCardClass(),
+        ],
+      )
+    );
+  }
+}
 
+class MyCardClass extends StatefulWidget {
+  const MyCardClass({Key? key}) : super(key: key);
+
+  @override
+  State<MyCardClass> createState() => _MyCardClassState();
+}
+
+class _MyCardClassState extends State<MyCardClass> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.network("https://expressdeliverymo.com/freight/wp-content/uploads/box.png", width: 50, height: 50),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Price: \$15345",
+                      style: TextStyle(fontSize:16),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Text(
+                "Delivered",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Divider(
+              thickness: 3,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Date ${DateTime.now().toString().substring(0, 10)}"),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.teal,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Review",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
