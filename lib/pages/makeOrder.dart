2752e68499a052955d8ef606424ba3f6e5663116
foad9776 <F_137071f>
2023-05-21 18:05:22 +0300

    fix on make order page design

--- lib/pages/makeOrder.dart
+++ lib/pages/makeOrder.dart
@@ -255,40 +255,6 @@ class _Order extends State<MakeOrderPage> with TickerProviderStateMixin {
   }
 
 
-  int _selectedIndex = 1;
-  static const TextStyle optionStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Home Page',
-      style: optionStyle,
-    ),
-    Text(
-      'Order Page',
-      style: optionStyle,
-    ),
-    Text(
-      'Account',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-    switch (index) {
-      case 0:
-        Navigator.pushNamed(context, '/mainPage');
-        break;
-      case 1:
-        Navigator.pushNamed(context, '/order');
-        break;
-      case 2:
-        Navigator.pushNamed(context, '/more');
-        break;
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -321,6 +287,10 @@ class _Order extends State<MakeOrderPage> with TickerProviderStateMixin {
           ),
         ]),
         backgroundColor: Colors.blue,
+        leading:  IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -440,25 +410,6 @@ class _Order extends State<MakeOrderPage> with TickerProviderStateMixin {
             ],
           ),
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
-            label: 'Order',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: 'More',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
       ),
     );
   }
