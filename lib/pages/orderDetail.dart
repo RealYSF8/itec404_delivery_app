66d97e39150212159c6afd384b673e2a52b9b27e
foad9776 <F_137071f>
2023-05-22 11:36:23 +0300

    my orders page( order details)

--- lib/pages/orderDetail.dart
+++ lib/pages/orderDetail.dart
@@ -41,192 +41,199 @@ class _OrderDetailPage extends State<OrderDetail> {
         ),
         backgroundColor: Colors.blue,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          setState(() {
-
-          });
-        },
-        child: Icon(Icons.edit),
-        backgroundColor: Colors.grey[800],
-      ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Text(
-              "Order Details",
-              textAlign: TextAlign.left,
-              overflow: TextOverflow.clip,
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
-                fontStyle: FontStyle.normal,
-                fontSize: 40,
-                // color: Color(0xffffffff),
-              ),
-            ),SizedBox(height: 15.0),
-            Row(mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Source:',
-                style: TextStyle(
-                  color: Colors.grey,
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                ),
-              ),
-              SizedBox(width: 10.0),//puts an invisible box between two texts
-              Text(
-                'name',
-                style: TextStyle(
-                  color: Color(0xfffba808),
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),SizedBox(height: 20.0),
-              ],
-            ),SizedBox(height: 15.0),
+            ListTile(
+                subtitle: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                      child: ListTile(
+                      title: Padding(
+                        padding: EdgeInsets.fromLTRB(0, 10, 0, 10), //apply padding to LTRB, L:Left, T:Top, R:Right, B:Bottom
+                        child: Text(
+                          "Order Details",
+                          textAlign: TextAlign.start,
+                          overflow: TextOverflow.clip,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontStyle: FontStyle.normal,
+                            fontSize: 35,
+                            // color: Color(0xffffffff),
+                          ),
+                        ),
+                      ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Divider(
+                      thickness: 3,
+                    ),
+                      SizedBox(height: 10,),
+                      Row(mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Source:',
+                            style: TextStyle(
+                              color: Color(0xfffba808),
+                              letterSpacing: 2.0,
+                              fontSize: 25.0,
+                            ),
+                          ),
+                          SizedBox(width: 10.0),//puts an invisible box between two texts
+                          Text(
+                            'Blank',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              letterSpacing: 2.0,
+                              fontSize: 18.0,
+                            ),
+                          ),SizedBox(height: 20.0),
+                        ],
+                      ),SizedBox(height: 20.0),
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Destination:',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    letterSpacing: 2.0,
-                    fontSize: 28.0,
-                  ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Destination:',
+                            style: TextStyle(
+                              color: Color(0xfffba808),
+                              letterSpacing: 2.0,
+                              fontSize: 25.0,
+                            ),
+                          ),
+                          SizedBox(width: 10.0),//puts an invisible box between two texts
+                          Text(
+                            'Blank',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              letterSpacing: 2.0,
+                              fontSize: 18.0,
+                            ),
+                          ),SizedBox(height: 20.0),
+                        ],
+                      ),SizedBox(height: 15.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Length:',
+                            style: TextStyle(
+                              color: Color(0xfffba808),
+                              letterSpacing: 2.0,
+                              fontSize: 25.0,
+                            ),
+                          ),
+                          SizedBox(width: 10.0),//puts an invisible box between two texts
+                          Text(
+                            'Blank',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              letterSpacing: 2.0,
+                              fontSize: 18.0,
+                            ),
+                          ),SizedBox(height: 20.0),
+                        ],
+                      ),SizedBox(height: 15.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                              Text(
+                                'Width:',
+                                style: TextStyle(
+                                  color: Color(0xfffba808),
+                                  letterSpacing: 2.0,
+                                  fontSize: 25.0,
+                                ),
+                              ),
+                              SizedBox(width: 10.0),//puts an invisible box between two texts
+                              Text(
+                                'Blank',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  letterSpacing: 2.0,
+                                  fontSize: 18.0,
+                                ),
+                              ),SizedBox(height: 20.0),
+                            ],
+                          ),SizedBox(height: 15.0),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                'Height:',
+                                style: TextStyle(
+                                  color: Color(0xfffba808),
+                                  letterSpacing: 2.0,
+                                  fontSize: 25.0,
+                                ),
+                              ),
+                              SizedBox(width: 10.0),//puts an invisible box between two texts
+                              Text(
+                                'Blank',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  letterSpacing: 2.0,
+                                  fontSize: 18.0,
+                                ),
+                              ),SizedBox(height: 20.0),
+                            ],
+                          ),SizedBox(height: 15.0),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                'Price:',
+                                style: TextStyle(
+                                  color: Color(0xfffba808),
+                                  letterSpacing: 2.0,
+                                  fontSize: 25.0,
+                                ),
+                              ),
+                              SizedBox(width: 10.0),//puts an invisible box between two texts
+                              Text(
+                                'Blank',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  letterSpacing: 2.0,
+                                  fontSize: 18.0,
+
+                                ),
+                              ),SizedBox(height: 20.0),
+                            ],
+                          ),SizedBox(height: 15.0),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                'Status:',
+                                style: TextStyle(
+                                  color: Color(0xfffba808),
+                                  letterSpacing: 2.0,
+                                  fontSize: 25.0,
+                                ),
+                              ),
+                              SizedBox(width: 10.0),//puts an invisible box between two texts
+                              Text(
+                                'Blank',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  letterSpacing: 2.0,
+                                  fontSize: 18.0,
+                                ),
+                              ),SizedBox(height: 20.0),
+                            ],
+                      ),
+                  ],
                 ),
-                SizedBox(width: 10.0),//puts an invisible box between two texts
-                Text(
-                  'name',
-                  style: TextStyle(
-                    color: Color(0xfffba808),
-                    letterSpacing: 2.0,
-                    fontSize: 28.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),SizedBox(height: 20.0),
-              ],
-            ),SizedBox(height: 15.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Length:',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    letterSpacing: 2.0,
-                    fontSize: 28.0,
-                  ),
-                ),
-                SizedBox(width: 10.0),//puts an invisible box between two texts
-                Text(
-                  'name',
-                  style: TextStyle(
-                    color: Color(0xfffba808),
-                    letterSpacing: 2.0,
-                    fontSize: 28.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),SizedBox(height: 20.0),
-              ],
-            ),SizedBox(height: 15.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Width:',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    letterSpacing: 2.0,
-                    fontSize: 28.0,
-                  ),
-                ),
-                SizedBox(width: 10.0),//puts an invisible box between two texts
-                Text(
-                  'name',
-                  style: TextStyle(
-                    color: Color(0xfffba808),
-                    letterSpacing: 2.0,
-                    fontSize: 28.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),SizedBox(height: 20.0),
-              ],
-            ),SizedBox(height: 15.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Height:',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    letterSpacing: 2.0,
-                    fontSize: 28.0,
-                  ),
-                ),
-                SizedBox(width: 10.0),//puts an invisible box between two texts
-                Text(
-                  'name',
-                  style: TextStyle(
-                    color: Color(0xfffba808),
-                    letterSpacing: 2.0,
-                    fontSize: 28.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),SizedBox(height: 20.0),
-              ],
-            ),SizedBox(height: 15.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Price:',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    letterSpacing: 2.0,
-                    fontSize: 28.0,
-                  ),
-                ),
-                SizedBox(width: 10.0),//puts an invisible box between two texts
-                Text(
-                  'name',
-                  style: TextStyle(
-                    color: Color(0xfffba808),
-                    letterSpacing: 2.0,
-                    fontSize: 28.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),SizedBox(height: 20.0),
-              ],
-            ),SizedBox(height: 15.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Status:',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    letterSpacing: 2.0,
-                    fontSize: 28.0,
-                  ),
-                ),
-                SizedBox(width: 10.0),//puts an invisible box between two texts
-                Text(
-                  'name',
-                  style: TextStyle(
-                    color: Color(0xfffba808),
-                    letterSpacing: 2.0,
-                    fontSize: 28.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),SizedBox(height: 20.0),
-              ],
-            ),SizedBox(height: 15.0),
+              ),
+                    ),
+              ),
+            ),
+          SizedBox(height: 15.0),
           ],
         ),
       ),
