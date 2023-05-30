b5f3eb12150b6a608d1ab83aeb9a426544b57268
foad9776 <F_137071f>
2023-05-30 12:34:53 +0300

    become carrier fixed

--- lib/pages/courrier.dart
+++ lib/pages/courrier.dart
@@ -1,18 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:io';
+import 'package:dotted_border/dotted_border.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:path/path.dart' as Path;
+import 'dart:async';
+import 'package:flutter/services.dart';
 
 class Courrier extends StatefulWidget {
   @override
   _CourrierState createState() => _CourrierState();
 }
 
-class _CourrierState extends State<Courrier> {
+class _CourrierState extends State<Courrier> with TickerProviderStateMixin{
   late TextEditingController textEditingController;
   late bool isDarkMode;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+  File? _selectedImage;
+  String? _userId;
+  String? _Name;
+
+  String _image = 'https://ouch-cdn2.icons8.com/84zU-uvFboh65geJMR5XIHCaNkx-BZ2TahEpE9TpVJM/rs:fit:784:784/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvODU5/L2E1MDk1MmUyLTg1/ZTMtNGU3OC1hYzlh/LWU2NDVmMWRiMjY0/OS5wbmc.png';
+  late AnimationController loadingController;
+
+  List<File?> _file = [];
+  List<PlatformFile?> _platformFile = [];
+
+
+  // late final file;
+  selectFile() async {
+    // file = null;
+    final file = await FilePicker.platform.pickFiles(
+        type: FileType.custom,
+        allowedExtensions: ['png', 'jpg', 'jpeg'],
+        allowMultiple: true
+    );
+    _platformFile = [];
+    _file = [];
+    if (file != null) {
+
+      for (int i = 0; i < file.files.length; i++) {
+        setState(() {
+          _file.add(File(file.files[i].path!));
+          _platformFile.add(file.files[i]);
+        });
+        await _uploadImage();
+      }
+    }
+
+    loadingController.forward();
+  }
 
   @override
   void initState() {
+    loadingController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 10),
+    )..addListener(() { setState(() {}); });
+    super.initState();
     super.initState();
     textEditingController = TextEditingController();
     getThemeMode();
@@ -23,6 +75,46 @@ class _CourrierState extends State<Courrier> {
     textEditingController.dispose();
     super.dispose();
   }
+
+
+
+
+  Future<String> _uploadImage() async {
+    try {
+      FirebaseStorage storage = FirebaseStorage.instance;
+      Reference storageReference = storage.refFromURL('gs://itec404deliveryapp.appspot.com');
+      String fileName = Path.basename(_selectedImage!.path);
+      Reference imageReference = storageReference.child('images/$fileName');
+      UploadTask uploadTask = imageReference.putFile(_selectedImage!);
+
+      TaskSnapshot taskSnapshot = await uploadTask;
+
+      String imageUrl = await taskSnapshot.ref.getDownloadURL();
+      print("Image uploaded successfully. Download URL: $imageUrl");
+
+      return imageUrl;
+    } catch (e) {
+      print("Error uploading image: $e");
+      throw e; // Re-throw the error to be handled by the caller
+    }
+  }
+
+
+
+  Future<void> _pickImage() async {
+    try {
+      PickedFile? pickedFile = await ImagePicker().getImage(source: ImageSource.gallery);
+      if (pickedFile != null) {
+        setState(() {
+          _selectedImage = File(pickedFile.path);
+        });
+        print("Selected image: $_selectedImage");
+      }
+    } on PlatformException catch (e) {
+      print('Error picking image: $e');
+    }
+  }
+
 
   void saveApplication() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -93,22 +185,34 @@ class _CourrierState extends State<Courrier> {
     return Scaffold(
       backgroundColor: isDarkMode ? Colors.grey[900] : Colors.white,
       appBar: AppBar(
-        elevation: 4,
         centerTitle: false,
-        backgroundColor: isDarkMode ? Colors.grey[700] : const Color(0xff3a57e8),
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.zero,
+        title: Row(
+          children: <Widget>[
+            Text(
+              "Courier",
+              textAlign: TextAlign.start,
+              overflow: TextOverflow.clip,
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontStyle: FontStyle.normal,
+                fontSize: 22,
+                color: Color(0xffffffff),
+              ),
+            ),
+            Text(
+              "Application",
+              textAlign: TextAlign.start,
+              overflow: TextOverflow.clip,
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontStyle: FontStyle.normal,
+                fontSize: 22,
+                color: Color(0xfffba808),
+              ),
+            ),
+          ],
         ),
-        leading: IconButton(
-          icon: Icon(
-            Icons.arrow_back,
-            color: isDarkMode ? Colors.white : const Color(0xff212435),
-            size: 24,
-          ),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
+        backgroundColor: Colors.blue,
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
@@ -117,7 +221,7 @@ class _CourrierState extends State<Courrier> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Padding(
-                padding: const EdgeInsets.only(bottom: 30),
+                padding: const EdgeInsets.only(top:30,bottom: 15),
                 child: Text(
                   "Become a Courier",
                   textAlign: TextAlign.start,
@@ -152,7 +256,114 @@ class _CourrierState extends State<Courrier> {
                   ),
                 ),
               ),
-              SizedBox(height: 66),
+              SizedBox(height: 20),
+              GestureDetector(
+                onTap: selectFile,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+                  child: DottedBorder(
+                    borderType: BorderType.RRect,
+                    radius: const Radius.circular(10),
+                    dashPattern: const [10, 4],
+                    strokeCap: StrokeCap.round,
+                    color: Colors.blue.shade400,
+                    child: Container(
+                      width: double.infinity,
+                      height: 100,
+                      decoration: BoxDecoration(
+                          color: Colors.blue.shade50.withOpacity(.3),
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(Iconsax.folder_open, color: Colors.blue, size: 35,),
+                          const SizedBox(height: 5),
+                          Text('Select your file', style: TextStyle(fontSize: 15, color: Colors.grey.shade400),),
+                          Text('File should be jpg, png', style: TextStyle(fontSize: 10, color: Colors.grey.shade500),),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              _platformFile.isNotEmpty
+                  ? Container(
+                // padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+                  padding: const EdgeInsets.only(left:10, top:0, right:10, bottom:10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Selected File',
+                        style: TextStyle(color: Colors.grey.shade400, fontSize: 15, ),),
+                      const SizedBox(height: 10,),
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.shade200,
+                                offset: const Offset(0, 1),
+                                blurRadius: 3,
+                                spreadRadius: 2,
+                              )
+                            ]
+                        ),
+                        child:_platformFile.isNotEmpty?
+                        ListView.builder(
+                            physics:const NeverScrollableScrollPhysics(),
+                            shrinkWrap:true,
+                            itemCount: _platformFile.length,
+                            itemBuilder: (context, index){
+                              return Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 5.0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    ClipRRect(
+                                        borderRadius: BorderRadius.circular(8),
+                                        child: Image.file(_file[index]!, width: 80, height: 60, fit: BoxFit.fill,)
+                                    ),
+                                    const SizedBox(width: 10),
+                                    Expanded(
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Text(_platformFile[index]!.name,
+                                            style: const TextStyle(fontSize: 13, color: Colors.black),),
+                                          const SizedBox(height: 5,),
+                                          Text('${(_platformFile[index]!.size / 1024).ceil()} KB',
+                                            style: TextStyle(fontSize: 13, color: Colors.grey.shade500),
+                                          ),
+                                          const SizedBox(height: 5,),
+                                          Container(
+                                              height: 5,
+                                              clipBehavior: Clip.hardEdge,
+                                              decoration: BoxDecoration(
+                                                borderRadius: BorderRadius.circular(5),
+                                                color: Colors.blue.shade50,
+                                              ),
+                                              child: LinearProgressIndicator(
+                                                value: loadingController.value,
+                                              )
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    const SizedBox(width: 10,),
+                                  ],
+                                ),
+                              );
+                            }
+                        )
+                            : Container(),
+                      ),
+                    ],
+                  ))
+                  : Container(),
               MaterialButton(
                 onPressed: saveApplication,
                 color: isDarkMode ? const Color(0xfff80707) : const Color(0xff3a57e8),
