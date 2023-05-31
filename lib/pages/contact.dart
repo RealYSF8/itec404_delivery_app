32242811cd813797388d0f73497ba675c5fb5ade
aboudehalimi <boudihalimi@gmail.com>
2023-05-31 14:26:29 +0300

    i added 3 more categories

--- lib/pages/contact.dart
+++ lib/pages/contact.dart
@@ -62,52 +62,239 @@ class _ContactState extends State<Contact> {
       ),
 
 
-    body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(16),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(16, 16, 16, 20),
+        child: SingleChildScrollView(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
             children: [
-              ImageIcon(
-                NetworkImage(
-                    "https://cdn1.iconfinder.com/data/icons/feather-2/24/message-circle-512.png"),
-                size: 60,
-                color: Color(0xffd81706),
-              ),
-              SizedBox(height: 30),
-              Text(
-                "Contact Us",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24,
-                  color: _isDarkMode ? Colors.white : Colors.grey[900],
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
+                child: Text(
+                  "Contact Us",
+                  textAlign: TextAlign.start,
+                  overflow: TextOverflow.clip,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontStyle: FontStyle.normal,
+                    fontSize: 24,
+                    color: Color(0xff3a57e8),
+                  ),
                 ),
               ),
-              SizedBox(height: 30),
-              Text(
-                "Abdallah Halimi (19700280) \nFoad Farahbod (19701110) \nMedhat Haddad (19700568) \nYoussef Binni (18700233)",
-
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 14,
-                  color: _isDarkMode ? Colors.white : Colors.grey[900],
+              Container(
+                margin: EdgeInsets.all(0),
+                padding: EdgeInsets.all(0),
+                width: MediaQuery.of(context).size.width,
+                height: 400,
+                decoration: BoxDecoration(
+                  color: Color(0xffffffff),
+                  shape: BoxShape.rectangle,
+                  borderRadius:
+                  BorderRadius.only(topRight: Radius.circular(140.0)),
+                  border: Border.all(color: Color(0xff3a57e8), width: 1),
                 ),
-              ),
-              SizedBox(height: 30),
-              Text(
-                " 19700280@emu.edu.tr \n 19701110@emu.edu.tr \n 19700568@emu.edu.tr \n 18700233@emu.edu.tr",
-                style: TextStyle(
-                  fontSize: 14,
-                  color: _isDarkMode ? Colors.white : Colors.grey[900],
-                ),
-              ),
-              SizedBox(height: 34),
-              Text(
-                "+90 5349932875 \n+90 5228627608",
-                style: TextStyle(
-                  fontSize: 14,
-                  color: _isDarkMode ? Colors.white : Colors.grey[900],
+                child: Padding(
+                  padding: EdgeInsets.all(32),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Icon(
+                            Icons.call,
+                            color: Color(0xff3a57e8),
+                            size: 24,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(8, 0, 0, 0),
+                            child: Text(
+                              "+90 5228627608",
+                              textAlign: TextAlign.start,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontStyle: FontStyle.normal,
+                                fontSize: 14,
+                                color: Color(0xff000000),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding:
+                        EdgeInsets.symmetric(vertical: 16, horizontal: 0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Icon(
+                              Icons.mail,
+                              color: Color(0xff3a57e8),
+                              size: 24,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(8, 0, 0, 0),
+                              child: Text(
+                                "swiftdelivery@gmail.com",
+                                textAlign: TextAlign.start,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontStyle: FontStyle.normal,
+                                  fontSize: 14,
+                                  color: Color(0xff000000),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Icon(
+                            Icons.account_circle,
+                            color: Color(0xff3a57e8),
+                            size: 24,
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Padding(
+                              padding: EdgeInsets.fromLTRB(8, 0, 0, 0),
+                              child: Text(
+                                "Members:",
+                                textAlign: TextAlign.start,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontStyle: FontStyle.normal,
+                                  fontSize: 14,
+                                  color: Color(0xff000000),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding:
+                        EdgeInsets.symmetric(vertical: 16, horizontal: 0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Icon(
+                              Icons.play_arrow,
+                              color: Color(0xff3a56e5),
+                              size: 24,
+                            ),
+                            Text(
+                              "Abdallah Halimi (19700280)",
+                              textAlign: TextAlign.start,
+                              overflow: TextOverflow.clip,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontStyle: FontStyle.normal,
+                                fontSize: 14,
+                                color: Color(0xff000000),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 1, 0, 16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Icon(
+                              Icons.play_arrow,
+                              color: Color(0xff3a57e8),
+                              size: 24,
+                            ),
+                            Text(
+                              "Medhat Haddad (19700568)",
+                              textAlign: TextAlign.start,
+                              overflow: TextOverflow.clip,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontStyle: FontStyle.normal,
+                                fontSize: 14,
+                                color: Color(0xff000000),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Icon(
+                            Icons.play_arrow,
+                            color: Color(0xff3956e6),
+                            size: 24,
+                          ),
+                          Text(
+                            "Foad Farahbod (19701110)",
+                            textAlign: TextAlign.start,
+                            overflow: TextOverflow.clip,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontStyle: FontStyle.normal,
+                              fontSize: 14,
+                              color: Color(0xff000000),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 16, 0, 0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Icon(
+                              Icons.play_arrow,
+                              color: Color(0xff3956e6),
+                              size: 24,
+                            ),
+                            Text(
+                              "Youssef Binni (18700233)",
+                              textAlign: TextAlign.start,
+                              overflow: TextOverflow.clip,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontStyle: FontStyle.normal,
+                                fontSize: 14,
+                                color: Color(0xff000000),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
